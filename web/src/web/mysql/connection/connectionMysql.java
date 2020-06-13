8188bab43b336d27053fd79e7777f6fa8e89eeed
edgarMolina94 <edgar.molina@integritas.mx>
2020-06-13 14:35:25 -0500

    f3

---
+++ web/src/web/mysql/connection/connectionMysql.java
@@ -0,0 +1,38 @@
+package web.mysql.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class connectionMysql {
+	Connection connection = null;
+	PreparedStatement statement = null;
+	ResultSet rs = null;
+	
+	public connectionMysql(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			try {
+				connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/PROMED?serverTimezone=UTC","root","mysql");
+			} catch (SQLException e) {
+				System.out.println("Error de conexión");
+				e.printStackTrace();
+			}
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error de driver");
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean exist(String sql) throws SQLException {
+		
+		Statement sentencia = connection.createStatement();
+		rs = sentencia.executeQuery(sql);
+		return rs.next();
+		
+	}
+}
+
